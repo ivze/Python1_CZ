fc5f40a7bf7a86f1c83f583a749cd2372af08cbc
IvZe <ivonaz@centrum.cz>
2023-02-02 20:57:28 +0100

    xxx

--- Lesson04/cviceni.py
+++
@@ -1,9 +0,0 @@
-# Napiš funkci mult, která bude mít dva číselné parametry. Funkce oba parametry vynásobí a vrátí výsledek.
-
-
-def mult(a, b):  # parametry
-    return a * b
-
-vysledek = mult(8, 5)  # argumenty
-print(f"Vysledek je: {vysledek}")
-
