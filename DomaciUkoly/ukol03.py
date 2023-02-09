3129acdb9a21d2e9928a85fc7012a7002ee0a85f
IvZe <ivonaz@centrum.cz>
2023-02-09 09:29:01 +0100

    3 ukol

--- DomaciUkoly/ukol03.py
+++ DomaciUkoly/ukol03.py
@@ -5,39 +5,6 @@ body = json.loads(text)
 print(body)
 
 
-# prospech = {
-#     "Daniel Svoboda": "pass",
-#    "Anežka Benešová": "fail",
-#    "Andrea Vanková": "fail",
-#    "Robin Blažek": "pass",
-#    "Renáta Tichá": "pass",
-#    "Matyáš Vanek": "fail",
-#    "Tereza Procházková": "fail",
-#    "Luboš Cerný": "pass",
-#    "Vasyl Novotný": "pass",
-#    "Jaroslav Polák": "fail",
-#    "Dušan Kríž": "pass",
-#    "Vlasta Kadlecová": "fail",
-#    "Zdenka Soukupová": "pass",
-#    "Igor Krejcí": "pass",
-#    "Stanislav Vanek": "pass",
-#    "Julie Poláková": "fail",
-#    "Veronika Fialová": "fail",
-#    "Kvetoslava Dvoráková": "fail",
-#    "Ladislav Cermák": "pass",
-#    "Dana Marková": "pass",
-#    "Miloš Horák": "pass",
-#    "Štefan Jelínek": "fail",
-#    "Miloš Veselý": "fail",
-#    "Aleš Kríž": "fail",
-#    "Marcela Machová": "fail",
-#    "Blanka Kucerová": "pass",
-#    "Šárka Marešová": "pass",
-#    "Dalibor Kadlec": "fail",
-#    "Robert Pospíšil": "fail"
-#    }
-
-
 prospech = {}
 
 for zak in body:
@@ -46,6 +13,8 @@ for zak in body:
     else:
         prospech[zak] = "fail"
 
+
 with open('prospech.json', mode='w', encoding='utf-8') as soubor:
-    soubor.write(json.dumps(body))
-print(json.dumps(prospech))
+    json.dump(body, soubor)
+
+print(prospech)
