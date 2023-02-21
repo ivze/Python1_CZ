25dff333c5e1ec14c28bce8186ffb1e0a161d782
IvZe <ivonaz@centrum.cz>
2023-02-21 19:19:05 +0100

    ukol5

--- DomaciUkoly/ukol06.py
+++ DomaciUkoly/ukol06.py
@@ -1,8 +1,8 @@
 import re
 
-# 1. Datum
+# 1. Platny vyraz
 platny_regularni_vyraz = re.compile(r"\d\d?(.|. |\/)\d\d?(.|. |\/)\d{4}")
 
-# 2. Adresa
+# 2. Posledni radek adresy
 posledni_radek_adresy_regularni_vyraz = re.compile(r"\d{3} \d{2} +(\w+ \w+ \w+|\w+ \w+|\w+)")
 
